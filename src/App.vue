b5c5c0ef0f7a433886ab8252a1f1548a38e9c4ec
ChangJoo Park <pcjpcj2@gmail.com>
2017-02-21 21:23:10 +0900

    Add Monaco Editor

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <MonacoEditor height="600" language="typescript" :code="code" :editorOptions="options" @mounted="onMounted" @codeChange="onCodeChange">
+    </MonacoEditor>
+  </div>
+</template>
+
+<script>
+  import MonacoEditor from 'vue-monaco-editor'
+
+  export default {
+    name: 'app',
+    components: {
+      MonacoEditor
+    },
+    data () {
+      return {
+        code: '// Type away! \n',
+        options: {
+          selectOnLineNumbers: false
+        }
+      }
+    },
+    methods: {
+      onMounted (editor) {
+        this.editor = editor
+      },
+      onCodeChange (editor) {
+        console.log(editor.getValue())
+      }
+    }
+  }
+
+</script>
+
+<style>
+</style>
